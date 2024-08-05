9be4b59039a6925a2dcfc7dfd9a83297e364e7e5
Charles <charlesbruno2010@hotmail.com>
2024-08-05 18:42:06 -0300

    feat challenge answer

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -12,8 +12,12 @@ export const getPosts = async () => {
 
 export const getPostBySlug = async (id) => {
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    const {data} = await api.get(`/posts?id=eq.${id}`)
 
-    return {}
+    console.log(data)
+    if(data){
+        return data[0];
+    }
+
+    return null
 }
